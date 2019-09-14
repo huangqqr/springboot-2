7e2508a0ab124b12f4fe0deb4a4f5859cc6059e0
huang_bo <huang_bo@leapmotor.com>
2019-09-14 16:21:02 +0800

    jpa

---
+++ springboot_jpa/src/main/java/com/hb/springboot/dao/Article.java
@@ -0,0 +1,39 @@
+package com.hb.springboot.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="article")
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false,length = 32)
+    private String author;
+
+    @Column(nullable = false, unique = true,length = 32)
+    private String title;
+
+    @Column(length = 512)
+    private String content;
+
+    private Date createTime;
+
+
+}
---
+++ springboot_jpa/src/main/java/com/hb/springboot/service/ArticleRestService.java
@@ -0,0 +1,19 @@
+package com.hb.springboot.service;
+
+
+import com.hb.springboot.model.ArticleVO;
+
+import java.util.List;
+
+public interface ArticleRestService {
+
+     ArticleVO saveArticle(ArticleVO article);
+
+     void deleteArticle(Long id);
+
+     void updateArticle(ArticleVO article);
+
+     ArticleVO getArticle(Long id);
+
+     List<ArticleVO> getAll();
+}
