f9e9505dce5d9525b163e804214f4e5a087f6c6c
huang_bo <huang_bo@leapmotor.com>
2019-09-14 12:13:58 +0800

    统一返回

---
+++ springboot_common_constant/src/main/java/com/hb/springboot/SpringbootCommonConstantApplication.java
@@ -0,0 +1,13 @@
+package com.hb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommonConstantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCommonConstantApplication.class, args);
+    }
+
+}
