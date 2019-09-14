0e3f52599719d6a253ae8ea52fe7f7d7473ace38
huang_bo <huang_bo@leapmotor.com>
2019-09-14 12:33:40 +0800

    PO转VO

---
+++ src/main/java/com/hb/springboot/utils/DozerUtils.java
@@ -0,0 +1,30 @@
+package com.hb.springboot.utils;
+
+import com.google.common.collect.Lists;
+import org.dozer.DozerBeanMapper;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+* @Description:  将list的PO转换为list的VO
+* @Param:
+* @return:
+* @Author: huangbo
+* @Date: 2019/9/14
+*/
+public class DozerUtils {
+
+    static DozerBeanMapper dozerBeanMapper = new DozerBeanMapper();
+
+    public static <T> List<T> mapList(Collection sourceList, Class<T> destinationClass){
+        List destinationList = Lists.newArrayList();
+        for (Iterator i$ = sourceList.iterator(); i$.hasNext();){
+            Object sourceObject = i$.next();
+            Object destinationObject = dozerBeanMapper.map(sourceObject, destinationClass);
+            destinationList.add(destinationObject);
+        }
+        return destinationList;
+    }
+}
