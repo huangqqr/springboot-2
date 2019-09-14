b1328dd0efb9707352c36a685850a76ddb8ea098
huang_bo <huang_bo@leapmotor.com>
2019-09-14 12:15:28 +0800

    JDBC

---
+++ springboot_jdbc/src/main/java/com/hb/springboot/model/Article.java
@@ -0,0 +1,26 @@
+package com.hb.springboot.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Article {
+
+    private Long id;
+    private String author;
+    private String title;
+    private String content;
+    private Date createTime;
+    private List<Reader> reader;
+
+
+}
